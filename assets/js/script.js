fc3bc93201593746faf1d01c29f89dff4482829e
Anjuman85 <anjumanbds22@gmail.com>
2022-11-11 11:12:41 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displaysAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displaysMultiplyQuestion() [
+    
+]
