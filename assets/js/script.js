0ca31e25c6cf6b9881ee1faad4e15e12db278474
Anjuman85 <anjumanbds22@gmail.com>
2022-11-12 17:28:02 +0000

    Add code for subtraction game

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,6 +34,8 @@ function runGame(gameType) {
         displaysAdditionQuestion(num1, num2);
     } else if (gameType === "multiply"){
         displaysMultiplyQuestion(num1,num2);
+    } else if (gameType === "subtract") {
+        displaySubtractQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw`Unknown game type: ${gameType} .Aborting!`;
@@ -75,8 +77,10 @@ function calculateCorrectAnswer() {
 
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
-    }else if (operator === "x") {
+    } else if (operator === "x") {
         return[operand1 * operand2, "multiply"];
+    } else if (operator === "-") {
+         return[operand1 - operand2, "subtract"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}.Aborting!`;
@@ -110,8 +114,11 @@ function displaysAdditionQuestion(operand1, operand2) {
     document.getElementById('operator').textContent = "+";
 }
 
-function displaySubtractQuestion() {
+function displaySubtractQuestion(operand1, operand2) {
 
+ document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+ document.getElementById("operand1").textContent = operand1 > operand2 ? operand2 : operand1;   
+   document.getElementById('operator').textContent = "-";
 }
 
 function displaysMultiplyQuestion(operand1, operand2) {
