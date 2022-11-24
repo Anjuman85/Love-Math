34f29fa1597d99d958a596bec29f321cb215f473
Anjuman85 <anjumanbds22@gmail.com>
2022-11-24 16:46:23 +0000

    Add some change structure of game functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -120,7 +120,7 @@ function incrementWrongAnswer() {
     document .getElementById("incorrect").innerText = ++oldScore;
 
 }
-function displaysAdditionQuestion(operand1, operand2) {
+function displayAdditionQuestion(operand1, operand2) {
     
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
@@ -134,7 +134,7 @@ function displaySubtractQuestion(operand1, operand2) {
    document.getElementById('operator').textContent = "-";
 }
 
-function displaysMultiplyQuestion(operand1, operand2) {
+function displayMultiplyQuestion(operand1, operand2) {
 
  document.getElementById('operand1').textContent = operand1;
  document.getElementById('operand2').textContent = operand2;
